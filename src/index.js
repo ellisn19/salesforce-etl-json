7639ff07615efa1861bb00dc0f6a7a325090411e
ellisn19 <112054165+ellisn19@users.noreply.github.com>
2025-08-06 09:49:09 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-// index.js
-const runETL = require('./etl.js');
+const runETL = require('./etl');
 
-(async () => {
-	try {
-		await runETL();
-		console.log('ETL process completed successfully.');
-	} catch (error) {
-		console.error('ETL process failed:', error);
+async function main() {
+	const args = process.argv.slice(2); // skip `node` and script name
+	if (args.length === 0) {
+		console.error('Please provide a query name as an argument.');
 		process.exit(1);
 	}
-})();
+	const queryName = args[0];
+
+	try {
+		await runETL(queryName);
+	} catch (err) {
+		console.error('ETL process failed:', err);
+		process.exit(1);
+	}
+}
+
+main();
